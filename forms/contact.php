54c22b29a1815eda1897a2627dde5c36a24c9c34
gentiosmanaj <gentiosmanaj@outlook.com>
2021-07-13 16:28:14 +0200

    email

--- forms/contact.php
+++ forms/contact.php
@@ -24,14 +24,51 @@
   $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
+  
+  // $contact->smtp = array(
+  //   'host' => 'github.com',
+  //   'username' => '',
+  //   'password' => '',
+  //   'port' => ''
+  // );
+
+  $config['protocol'] = 'smtp';
+				$config['smtp_host'] = 'ssl://smtp.googlemail.com';
+				$config['smtp_port']    = '465';
+				$config['smtp_timeout'] = '60';
+				$config['smtp_user']    = 'genti.osmanaj3@gmail.com';
+				$config['smtp_pass']    = 'yllka28112017';
+				$config['charset']    = 'utf-8';
+				$config['newline']    = "\r\n";
+				$config['mailtype'] = 'text'; // or html
+				$config['validation'] = false; // bool whether to validate email or not      
+
+				$this->email->initialize($config);
+
+		    	$name=$_POST('name');
+		    	$from=$_POST('email');
+		    	$subject=$_POST('subject');
+		    	$message=$_POST('message');
+		    	$this->email->to('genti.osmanaj3@gmail.com');
+				$this->email->from($from,'LandiFrigo');
+				$this->email->subject($subject);
+				$this->email->message($message.'from '.$name);
+
+				if($this->email->send()){
+					$this->session->set_flashdata('email_send','Your email was sent successfully');
+		    		redirect('');
+				}else{
+				   $this->session->set_flashdata('email_dontsend','Your email hasent been sent');
+		    		redirect('');
+				}
+
+				$this->session->set_flashdata('email_send','Your email was sent successfully');
+		    	redirect('');
+		    }
+	}
+}
+  
+  
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
